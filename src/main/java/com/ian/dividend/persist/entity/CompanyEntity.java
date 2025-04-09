a0ed06fff34eb696ba67006bc46df0259e904ddd
YB-Taekwon <loundo001@gmail.com>
2025-04-09 23:49:58 +0900

    테이블 설계

---
+++ src/main/java/com/ian/dividend/persist/entity/CompanyEntity.java
@@ -0,0 +1,23 @@
+package com.ian.dividend.persist.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity(name = "COMPANY")
+public class CompanyEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String ticker;
+
+    private String name;
+}
---
+++ src/main/java/com/ian/dividend/persist/entity/DividendEntity.java
@@ -0,0 +1,27 @@
+package com.ian.dividend.persist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity(name = "DIVIDEND")
+public class DividendEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long companyId;
+    private LocalDateTime date;
+    private String dividend;
+}
